d15a8db342d3223605d63166f946197b8d8db676
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-09-30 09:17:00 +0000

    using windows forms runtime impl.

---
+++ trunk/QCV/ShowImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using QCV.Base.Extensions;
+
+namespace QCV {
+
+  [Base.Addins.Addin]
+  public class ShowImage : Base.IFilter {
+
+    private string _bag_name = "source";
+    public string BagName {
+      get { return _bag_name; }
+      set { _bag_name = value; }
+    }
+
+    private string _show_name = "image";
+    public string ShowName {
+      get { return _show_name; }
+      set { _show_name = value; }
+    }
+
+    public void Execute(QCV.Base.Bundle b, System.ComponentModel.CancelEventArgs e) {
+      Image<Bgr, byte> i = b.FetchImage(_bag_name);
+      Base.Runtime r = b.FetchRuntime();
+      r.Show(_show_name, i);
+    }
+  }
+}
