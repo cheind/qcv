26dc2fcb946bc0462f5f1bf8af62d87448b87eaa
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-01 08:14:12 +0000

    refactored scripting to support multiple languages.

--- trunk/QCV.Base/Scripting.cs
+++ trunk/QCV.Base/Scripting.cs
@@ -4,21 +4,56 @@ using System.Linq;
 using System.Text;
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
+using Microsoft.VisualBasic;
+using Microsoft.VisualC;
 
 namespace QCV.Base {
   public class Scripting {
+    
+    public enum Language {
+      CSharp,
+      VisualBasic,
+      Cpp
+    }
 
-    public static CompilerResults Compile(IEnumerable<string> sources, IEnumerable<string> refs) {
+    private CodeDomProvider _provider;
 
-      CSharpCodeProvider provider = new CSharpCodeProvider();
+    public Scripting(Language language) {
+      switch (language) {
+        case Language.CSharp :
+          _provider = new CSharpCodeProvider();
+          break;
+        case Language.VisualBasic:
+          _provider = new VBCodeProvider();
+          break;
+        case Language.Cpp:
+          _provider = new CppCodeProvider();
+          break;
+      }
+    }
+
+    public CompilerResults Compile(IEnumerable<string> sources, IEnumerable<string> refs) {
 
       CompilerParameters props = new CompilerParameters(refs.ToArray());
       props.GenerateExecutable = false;
       props.GenerateInMemory = true;
       
-      return provider.CompileAssemblyFromFile(props, sources.ToArray());
+      return _provider.CompileAssemblyFromFile(props, sources.ToArray());
     }
 
+    public String FormatCompilerResults(CompilerResults cr) {
+      StringBuilder sb = new StringBuilder();
 
+      if (cr.Errors.Count > 0) {
+        sb.AppendLine("Build failed");
+        for (int i = 0; i < cr.Errors.Count; i++)
+          sb.AppendLine(i.ToString() + ": " + cr.Errors[i].ToString());
+      } else {
+        sb.AppendLine("Build succeeded");
+        sb.AppendLine("Compiler returned with result code: " + cr.NativeCompilerReturnValue.ToString());
+      }
+
+      return sb.ToString();
+    }
   }
 }
---
+++ trunk/QCV.ConsoleExample/HelloLogging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+using QCV.Base.Extensions;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using log4net;
+
+namespace QCV.ConsoleExample {
+
+  [Base.Addins.Addin]
+  public class HelloLogging : IExample {
+
+    public void Run(string[] args) {
+
+      QCV.Base.FilterList f = new QCV.Base.FilterList();
+      f.Add(
+        new QCV.Base.AnonymousFilter(
+          (b, ev) => {
+            ILog l = b.FetchLogger();
+            l.Debug("A debugging greeting message!");
+            ev.Cancel = true;
+          })
+      );
+     
+
+      QCV.Base.Runtime runtime = new QCV.Base.Runtime();
+      runtime.FPS = 30.0;
+      runtime.Run(f, new QCV.Base.ConsoleInteraction(), 10);
+    }
+  }
+}
