9badcfb79d8802223181f2937dd3f297c74f9091
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-08 18:10:34 +0000

    example for desing principle responsiveness.

---
+++ trunk/QCV.Tutorial/stay_responsive.cs
@@ -0,0 +1,37 @@
+﻿// qcv.exe -s stay_responsive.cs Tutorial.StayResponsive --run
+
+using System;
+using System.Collections.Generic;
+
+using QCV.Base;
+using QCV.Base.Addins;
+using QCV.Base.Extensions;
+
+namespace Tutorial {
+
+  [Addin]
+  public class StayResponsive : IFilter, IFilterListProvider {
+
+    public FilterList CreateFilterList(AddinHost host) {
+      return new FilterList() {
+        this
+      };
+    }
+
+    public void Execute(Dictionary<string, object> bundle) {
+
+      // If you have a long running operation, make sure 
+      // to stay responsive cancellation events.
+
+      while (true) {
+        // Test if a cancellation request is pending
+        if (bundle.GetRuntime().CancellationPending) {
+          return;
+        }
+
+        System.Threading.Thread.Sleep(50);
+      }
+    }
+
+  }
+}
