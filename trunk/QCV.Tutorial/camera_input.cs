0955bf78e4b7b91751556ef93b077e6c546bd04c
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-08 17:22:21 +0000

    added class libraries for examples and tutorial

---
+++ trunk/QCV.Tutorial/camera_input.cs
@@ -0,0 +1,25 @@
+// qcv.exe -s camera_input.cs Tutorial.CameraInput --run
+
+using System;
+using System.Collections.Generic;
+
+using QCV.Base;
+using QCV.Base.Addins;
+
+namespace Tutorial {
+
+  [Addin]
+  public class CameraInput : IFilterListProvider {
+    
+    // Create a new FilterList containing a camera filter
+    public FilterList CreateFilterList(AddinHost host) {
+      
+      QCV.Toolbox.Camera c = new QCV.Toolbox.Camera();
+      c.DeviceIndex = 0;
+      c.Name = "source";
+      
+      return new FilterList() {c};
+    }
+
+  }
+}
---
+++ trunk/QCV.Tutorial/event_notifications.cs
@@ -0,0 +1,33 @@
+// qcv.exe -s event_notifications.cs Tutorial.EventNotifications --run
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using QCV.Base;
+using QCV.Base.Addins;
+using QCV.Base.Extensions;
+
+namespace Tutorial {
+
+  [Addin]
+  public class EventNotifications : IFilter, IFilterListProvider {
+    
+    public FilterList CreateFilterList(AddinHost host) {
+      return new FilterList() {
+        this
+      };
+    }
+    
+    public void OnPrintGreeting(Dictionary<string, object> bundle) {
+      Console.WriteLine("Hello EventNotifications!");
+    }
+    
+    public void Execute(Dictionary<string, object> bundle) {
+      IDataInteractor idi = bundle.FetchInteractor();
+      // Process all pending events, supplying them with the current bundle information
+      idi.ExecutePendingEvents(this, bundle);
+    }
+    
+  }
+}
