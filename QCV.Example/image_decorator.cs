a66a27421104e0f87e10d2845adc42cdd624e90b
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-13 08:49:28 +0000

    step 1 image-decorator - hello world

---
+++ QCV.Example/image_decorator.cs
@@ -0,0 +1,30 @@
+﻿// qcv.exe -s image_decorator.cs Example.ImageDecorator --run
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+
+using QCV.Base;
+using QCV.Base.Addins;
+using QCV.Base.Extensions;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Example {
+
+  [Addin]
+  public class ImageDecorator : IFilter, IFilterListProvider {
+
+    public FilterList CreateFilterList(AddinHost host) {
+      return new FilterList() {
+        this,
+      };
+    }
+
+    public void Execute(Dictionary<string, object> b) {
+      Console.WriteLine("Hello World");
+    }
+  }
+}
