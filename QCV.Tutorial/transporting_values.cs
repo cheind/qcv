8f5ff6ba8a08709ba511dd93fce98e4b906c9a6c
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-13 08:49:04 +0000

    added from tutorial.

---
+++ QCV.Tutorial/transporting_values.cs
@@ -0,0 +1,37 @@
+﻿// qcv.exe -s transporting_values.cs Tutorial.TransportingValues --run
+
+using System;
+using System.Collections.Generic;
+
+using QCV.Base;
+using QCV.Base.Addins;
+using QCV.Base.Extensions;
+
+namespace Tutorial {
+
+  class AFilter : IFilter {
+    
+    public void Execute(Dictionary<string, object> bundle) {
+      bundle["A"] = DateTime.Now;
+    }
+  }
+
+  class BFilter : IFilter {
+
+    public void Execute(Dictionary<string, object> bundle) {
+      System.Console.WriteLine(String.Format("A:{0}", bundle.Get<DateTime>("A")));
+    }
+  }
+
+  [Addin]
+  public class TransportingValues : IFilterListProvider {
+
+    public FilterList CreateFilterList(AddinHost host) {
+      return new FilterList() {
+        new AFilter(),
+        new BFilter()
+      };
+    }
+
+  }
+}
