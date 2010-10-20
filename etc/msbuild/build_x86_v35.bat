de20d36f3e832c8c310e42c48b607b8dd46316de
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-20 07:47:18 +0000

    renaming of created installer.

--- etc/msbuild/build_x86_v35.bat
+++ etc/msbuild/build_x86_v35.bat
@@ -8,4 +8,5 @@
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild "..\..\qcv.sln" /p:TargetFrameworkVersion=v3.5 /p:Configuration=Release /p:Platform=x86 /t:rebuild /ToolsVersion:4.0
+ren "..\..\bin\x86\qcv_installer_x86.msi" "qcv_installer_net35_x86.msi"
 pause
