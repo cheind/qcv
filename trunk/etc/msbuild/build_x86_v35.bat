a1220e276aad48d31e1605ce13b7f1a42c9792ab
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-20 07:42:17 +0000

    added msbuild scripts.

---
+++ trunk/etc/msbuild/build_x86_v35.bat
@@ -0,0 +1,11 @@
+::
+:: QCV
+:: Copyright (c) Christoph Heindl, 2010
+::
+:: This batch file builds the entire QCV solution for the .NET 3.5 target framework.
+:: Requires Visual Studio 2010.
+::
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+msbuild "..\..\qcv.sln" /p:TargetFrameworkVersion=v3.5 /p:Configuration=Release /p:Platform=x86 /t:rebuild /ToolsVersion:4.0
+pause
