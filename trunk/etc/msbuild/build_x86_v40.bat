a1870267fe8fa0ec2b3a62312b28078d9ddcd136
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-20 07:47:18 +0000

    renaming of created installer.

--- trunk/etc/msbuild/build_x86_v40.bat
+++ trunk/etc/msbuild/build_x86_v40.bat
@@ -8,4 +8,5 @@
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild "..\..\qcv.sln" /p:TargetFrameworkVersion=v4.0 /p:Configuration=Release /p:Platform=x86 /t:rebuild /ToolsVersion:4.0
+ren "..\..\bin\x86\qcv_installer_x86.msi" "qcv_installer_net40_x86.msi"
 pause
