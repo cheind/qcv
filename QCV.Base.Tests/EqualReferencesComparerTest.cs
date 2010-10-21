6a0a89392aaf7de35b6c0bf231a0f8fc15fc07f9
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-21 07:28:21 +0000

    added nunit and inital test projects.

---
+++ QCV.Base.Tests/EqualReferencesComparerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace QCV.Base.Tests {
+
+  [TestFixture]
+  class EqualReferencesComparerTest {
+
+    class TestFilter : Base.IFilter {
+      public void Execute(Dictionary<string, object> b) {
+        throw new NotImplementedException();
+      }
+    }
+
+    [Test]
+    public void Equals() {
+
+      Base.EqualReferencesComparer e = new EqualReferencesComparer();
+
+      TestFilter a = new TestFilter();
+      TestFilter b = new TestFilter();
+
+      Assert.True(e.Equals(a, a));
+      Assert.True(e.Equals(b, b));
+      Assert.False(e.Equals(a, b));
+    }
+  }
+}
