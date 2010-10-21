954ce3475b42960dfe540c47ad5467bc76a0f584
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-21 12:05:44 +0000

    test added.

---
+++ trunk/QCV.Base.Tests/FixedTimeStepTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace QCV.Base.Tests {
+
+  [TestFixture]
+  class FixedTimeStepTest {
+
+    [Test]
+    public void Fixed() {
+      Stopwatch sw = new Stopwatch();
+      Base.FixedTimeStep fts = new FixedTimeStep();
+      fts.FPS = 30;
+      fts.Enabled = true;
+
+      sw.Start();
+      int count = 0;
+      while (sw.ElapsedMilliseconds < 1000) {
+        fts.UpdateAndWait();
+        count += 1;
+      }
+      sw.Stop();
+
+      Assert.AreEqual(30.0, count * 1.0, 2.0); 
+    }
+
+    [Test]
+    public void Disabled() {
+      Stopwatch sw = new Stopwatch();
+      Base.FixedTimeStep fts = new FixedTimeStep();
+      fts.FPS = 30;
+      fts.Enabled = false;
+
+      sw.Start();
+      int count = 0;
+      while (sw.ElapsedMilliseconds < 100) {
+        fts.UpdateAndWait();
+        count += 1;
+      }
+      sw.Stop();
+
+      Assert.Less(100.0, count);
+    }
+  }
+}
