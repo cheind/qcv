d2e7df52a91f772717444093242d6d81aeb450cd
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-21 11:57:22 +0000

    tests added.

---
+++ QCV.Base.Tests/MethodInfoScannerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace QCV.Base.Tests {
+  
+  [TestFixture]
+  class MethodInfoScannerTest {
+
+    class MyFilter : Base.IFilter {
+      public void OnClickMe(Dictionary<string, object> bundle) { }
+      public void OnClickMe2(Dictionary<string, object> bundle) { }
+      public void OnNotAnEvent(int wrong_type) { }
+      private void OnNotAnEvent2(Dictionary<string, object> bundle) { }
+
+      public void Execute(Dictionary<string, object> b) {
+        throw new NotImplementedException();
+      }
+    }
+
+    [Test]
+    public void FindEvents() {
+      MethodInfo[] mi = Base.MethodInfoScanner.FindEventMethods(new MyFilter());
+      mi = mi.OrderBy((m) => m.Name).ToArray();
+
+      Assert.AreEqual(2, mi.Length);
+      Assert.AreEqual("OnClickMe", mi[0].Name);
+      Assert.AreEqual("OnClickMe2", mi[1].Name);
+    }
+  }
+}
---
+++ QCV.Base.Tests/TypeFullNameComparerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace QCV.Base.Tests {
+
+  [TestFixture]
+  class TypeFullNameComparerTest {
+
+    [Test]
+    public void Compare() {
+      Type a = typeof(string);
+      Type b = typeof(string);
+
+      Base.TypeFullNameComparer c = new Base.TypeFullNameComparer();
+      Assert.True(c.Equals(a, b));
+      Assert.False(c.Equals(a, typeof(int)));
+
+      Assert.AreEqual(a.FullName.GetHashCode(), c.GetHashCode(a));
+    }
+  }
+}
