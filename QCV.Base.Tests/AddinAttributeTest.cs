30a05b9cb423c154eb32227d645378368c7824a6
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-21 08:55:57 +0000

    added tests

---
+++ QCV.Base.Tests/AddinAttributeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace QCV.Base.Tests {
+
+  [TestFixture]
+  class AddinAttributeTest {
+
+    [Base.Addin]
+    class CarriesAddin {
+    }
+
+    [Test]
+    public void HasAddin() {
+      Type t = typeof(CarriesAddin);
+      Assert.True(Attribute.IsDefined(t, typeof(QCV.Base.AddinAttribute)));
+    }
+
+  }
+}
---
+++ QCV.Base.Tests/TypeExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using QCV.Base.Extensions;
+
+namespace QCV.Base.Tests {
+
+  [TestFixture]
+  class TypeExtensionsTest {
+
+    [Base.Addin]
+    class CarriesAddin {
+    }
+
+    class CarriesNoAddin {
+    }
+
+    [Test]
+    public void IsAddin() {
+      Assert.True(typeof(CarriesAddin).IsAddin());
+      Assert.False(typeof(CarriesNoAddin).IsAddin());
+    }
+  }
+}
