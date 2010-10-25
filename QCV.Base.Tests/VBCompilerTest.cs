e4a06ea8ec0d84d2a6416195be4f1511e9387941
christoph.heindl <christoph.heindl@01a2a4ec-7636-277f-0789-769b57ea81c1>
2010-10-25 13:00:18 +0000

    more compiler tests.

--- QCV.Base.Tests/VBCompilerTest.cs
+++ QCV.Base.Tests/VBCompilerTest.cs
@@ -5,25 +5,26 @@ using System.Text;
 using NUnit.Framework;
 using QCV.Base.Compilation;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace QCV.Base.Tests {
-  
+
   [TestFixture]
-  class CSharpCompilerTest {
+  class VBCompilerTest {
 
     [Test]
     public void CanCompile() {
       CompilerSettings s = new CompilerSettings();
-      CSharpCompiler c = new CSharpCompiler(s);
-      Assert.True(c.CanCompileFile(@"..\..\etc\testdata\compiler\compile_ok.cs"));
+      VBCompiler c = new VBCompiler(s);
+      Assert.True(c.CanCompileFile(@"..\..\etc\testdata\compiler\compile_ok.vb"));
     }
 
     [Test]
     public void Compile() {
       CompilerSettings s = new CompilerSettings();
-      CSharpCompiler c = new CSharpCompiler(s);
+      VBCompiler c = new VBCompiler(s);
 
-      ICompilerResults cr = c.CompileFiles(new string[] { @"..\..\etc\testdata\compiler\compile_ok.cs" });
+      ICompilerResults cr = c.CompileFiles(new string[] { @"..\..\etc\testdata\compiler\compile_ok.vb" });
       Assert.True(cr.Success);
 
       Assembly a = cr.GetCompiledAssemblies().First();
@@ -41,11 +42,11 @@ namespace QCV.Base.Tests {
       CompilerSettings s = new CompilerSettings();
       VBCompiler c = new VBCompiler(s);
 
-      ICompilerResults cr = c.CompileFiles(new string[] { @"..\..\etc\testdata\compiler\compile_fail.cs" });
+      ICompilerResults cr = c.CompileFiles(new string[] { @"..\..\etc\testdata\compiler\compile_fail.vb" });
       Assert.False(cr.Success);
       string formatted_erros = cr.GetFormattedErrors();
-      Assert.True(formatted_erros.Contains("compile_fail.cs(5,0)"));
-
+      Assert.True(formatted_erros.Contains("compile_fail.vb(3,0)"));
+      
     }
 
   }
---
+++ etc/testdata/compiler/compile_fail.vb
@@ -0,0 +1,9 @@
+Namespace QCV.Test
+
+	Public Class TestCompilation;
+		Public Function GetString() As String
+			Return "Compilation Succeeded"
+		End Function
+	End Class
+
+End Namespace
